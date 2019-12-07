51922016e654bcaa88d75feba40c6e52c49b1577
Jonathan Dumaine <jonathan.dumaine@dumstruck.com>
2019-12-07 08:58:07 -0700

    useBoundingClientRect: Always return a rect object
    Makes working with the hook much easier so you don't need to null check the return value constantly.
    @rehooks/component-size and others do something similar

--- packages/boundingclientrect/src/useBoundingclientrect.ts
+++ packages/boundingclientrect/src/useBoundingclientrect.ts
@@ -2,6 +2,15 @@ import { useState, useEffect, MutableRefObject, useCallback } from "react";
 import { useMutationObserver } from "shared/useMutationObserver";
 import { useDidMount } from "shared/useDidMount";
 
+const EMPTY_RECT = {
+  width: 0,
+  height: 0,
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0
+}
+
 /**
  * useBoundingclientRect hook
  *
@@ -12,14 +21,14 @@ import { useDidMount } from "shared/useDidMount";
 function getBoundingClientRect(
   element: HTMLElement
 ): ClientRect | DOMRect | null {
-  return element.getBoundingClientRect();
+  return element ? element.getBoundingClientRect() : EMPTY_RECT;
 }
 
 function useBoundingclientrect(ref: MutableRefObject<HTMLElement | null>) {
-  const [value, setValue] = useState<ClientRect | DOMRect | null>(null);
+  const [value, setValue] = useState<ClientRect | DOMRect | null>(EMPTY_RECT);
 
   const update = useCallback(() => {
-    setValue(ref.current ? getBoundingClientRect(ref.current) : null);
+    setValue(getBoundingClientRect(ref.current));
   }, []);
 
   useDidMount(() => {
